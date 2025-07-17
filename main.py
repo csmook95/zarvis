efe8272cbc1376ea5a0fe617cbd45ee33874b143
csmook95 <csmook95@gmail.com>
2025-07-17 11:06:17 +0900

    feat: Gradio 기본 인터페이스 추가
    - 오디오 입력 및 텍스트 출력 UI 구성
    - 오디오 파일 경로를 텍스트 박스에 표시하는 기본 기능 구현

---
+++ main.py
@@ -0,0 +1,15 @@
+import gradio as gr
+
+with gr.Blocks() as demo:
+	def change_audio(audio_path):
+		return audio_path
+
+	# input_audio = gr.Audio(sources="microphone", type="filepath", label="마이크 입력")
+	input_audio = gr.Audio(type="filepath")
+	output_text = gr.Textbox(
+		label="음성 인식 결과", placeholder="여기에 음성 인식 결과가 표시됩니다.", interactive=False)
+
+	input_audio.change(change_audio, inputs=[input_audio], outputs=[output_text])
+
+
+demo.launch()
